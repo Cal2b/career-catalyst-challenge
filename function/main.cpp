fafb5dec378df5b4634696a66952ee49cdff25b0
Cal2b <100943016+Cal2b@users.noreply.github.com>
2022-03-07 16:41:18 +0300

    Create main.cpp

---
+++ function/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+ addition(int a ,int b){
+     return a+b;
+    }
+
+int main()
+{
+     int a=10;
+    int b=12;
+      int result =addition(a, b);
+      cout<<result<<endl;
+    return 0;
+}
